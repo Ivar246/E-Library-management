c47f40e0109e0b01332b7fbe448f14d2f5a6a7c8
Ravi Shrestha <ravistha869@gmail.com>
2023-12-25 19:15:16 +0545

    ...

--- src/entity/Review.ts
+++ src/entity/Review.ts
@@ -10,7 +10,7 @@ export class Review {
     @Column("text", {
         name: 'review'
     })
-    review: string
+    text: string
 
     @ManyToOne(() => User)
     @JoinColumn({ name: "user_id" })
